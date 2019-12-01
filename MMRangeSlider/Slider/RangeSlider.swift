298051b339289c1ab0c474d2dfb452697a85f2e6
Mikael <melkonyan2311@icloud.com>
2019-12-01 16:20:08 +0200

    Range slider empty file

---
+++ MMRangeSlider/Slider/RangeSlider.swift
@@ -0,0 +1,14 @@
+//
+//  RangeSlider.swift
+//  MMRangeSlider
+//
+//  Created by Mikael Melkonyan on 01.12.2019.
+//  Copyright © 2019 Mikael Melkonyan. All rights reserved.
+//
+
+import UIKit
+
+final class RangeSlider: UIView {
+    
+    
+}
