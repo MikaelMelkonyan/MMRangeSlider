78e02e56be5a1ecf5762b9bc0842cb6df945b6ce
Mikael <melkonyan2311@icloud.com>
2019-12-01 16:43:36 +0200

    Lines creation

--- MMRangeSlider/Slider/RangeSlider.swift
+++ MMRangeSlider/Slider/RangeSlider.swift
@@ -10,5 +10,68 @@ import UIKit
 
 final class RangeSlider: UIView {
     
+    private var backLine: UIView!
+    private var line: UIView!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        createSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        createSubviews()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        positionSubviews()
+    }
+}
+
+// MARK: - Subviews frame
+private extension RangeSlider {
+    
+    func positionSubviews() {
+        updateBackLineFrame()
+        updateLineFrame()
+    }
+    
+    func updateBackLineFrame() {
+        #warning("todo")
+        let left: CGFloat = 0
+        let right: CGFloat = 0
+        let top: CGFloat = 20
+        let height: CGFloat = 20
+        backLine.frame = CGRect(x: left, y: top, width: frame.width - left - right, height: height)
+    }
+    
+    func updateLineFrame() {
+        #warning("todo")
+        line.frame = CGRect(x: backLine.frame.minX, y: backLine.frame.minY, width: backLine.frame.width / 2, height: backLine.frame.height)
+    }
+}
+
+// MARK: - Subviews creation
+private extension RangeSlider {
+    
+    func createSubviews() {
+        createBackLine()
+        createLine()
+    }
+    
+    func createBackLine() {
+        backLine = UIView(frame: .zero)
+        backLine.backgroundColor = .black
+        addSubview(backLine)
+    }
+    
+    func createLine() {
+        line = UIView(frame: .zero)
+        line.backgroundColor = .brown
+        addSubview(line)
+    }
 }
